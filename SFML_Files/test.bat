3e36154ef1e82e6e57ce5b4430663dbe5b7f3e4b
Augbie <augiegould@gmail.com>
2023-11-29 00:15:12 -0500

    Created main screen and test.bat file

---
+++ SFML_Files/test.bat
@@ -0,0 +1,2 @@
+make
+main.exe | find /v ""
--- main.cpp
+++ main.cpp
@@ -25,24 +25,67 @@ main(){
     std::cout << p1.productURL << std::endl;
     std::cout << p1.imageURL << std::endl;
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    int i = 0;
+// SFML Shee Begins
+    // Create variables to be used
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Project_3", sf::Style::Close);
+    int WINDOW_HEIGHT = sf::VideoMode::getDesktopMode().height;
+    int WINDOW_WIDTH = sf::VideoMode::getDesktopMode().width;
+
+    
+    sf::RectangleShape SEARCH_MODE_SELECT_BOX(sf::Vector2f(WINDOW_WIDTH/4.67, WINDOW_HEIGHT/2.67));
+    SEARCH_MODE_SELECT_BOX.setFillColor(sf::Color::Transparent);
+    SEARCH_MODE_SELECT_BOX.setOutlineColor(sf::Color::White);
+    SEARCH_MODE_SELECT_BOX.setOutlineThickness(2);
+    SEARCH_MODE_SELECT_BOX.move(2,2);
+
+    sf::RectangleShape SEARCH_BOX(sf::Vector2f(WINDOW_WIDTH/1.4, WINDOW_HEIGHT/12));
+    SEARCH_BOX.setFillColor(sf::Color::Transparent);
+    SEARCH_BOX.setOutlineColor(sf::Color::White);
+    SEARCH_BOX.setOutlineThickness(2);
+    SEARCH_BOX.setPosition((WINDOW_WIDTH/4.67)+4, WINDOW_HEIGHT/20);
+
+    sf::RectangleShape GO_BOX(sf::Vector2f(WINDOW_HEIGHT/12, WINDOW_HEIGHT/12));
+    GO_BOX.setFillColor(sf::Color::Transparent);
+    GO_BOX.setOutlineColor(sf::Color::White);
+    GO_BOX.setOutlineThickness(2);
+    GO_BOX.setPosition((WINDOW_WIDTH/1.4)+(WINDOW_WIDTH/4.67)+6, WINDOW_HEIGHT/20);
+
+    sf::RectangleShape RESULTS_MAIN_OUTLINE(sf::Vector2f((WINDOW_WIDTH/1.4)-2, WINDOW_HEIGHT/1.4));
+    RESULTS_MAIN_OUTLINE.setFillColor(sf::Color::Transparent);
+    RESULTS_MAIN_OUTLINE.setOutlineColor(sf::Color::White);
+    RESULTS_MAIN_OUTLINE.setOutlineThickness(2);
+    RESULTS_MAIN_OUTLINE.setPosition((WINDOW_WIDTH/4.67)+6, (WINDOW_HEIGHT/20)+(WINDOW_HEIGHT/12)+4);
+
+    sf::RectangleShape PAGE_LEFT_BUTTON(sf::Vector2f(WINDOW_WIDTH/9.33, WINDOW_HEIGHT/12));
+    PAGE_LEFT_BUTTON.setFillColor(sf::Color::Transparent);
+    PAGE_LEFT_BUTTON.setOutlineColor(sf::Color::White);
+    PAGE_LEFT_BUTTON.setOutlineThickness(2);
+    PAGE_LEFT_BUTTON.setPosition((WINDOW_WIDTH/4.67)+6+(WINDOW_WIDTH/24), (WINDOW_HEIGHT/1.4)+(WINDOW_HEIGHT/20)+(WINDOW_HEIGHT/12)+8);
+    
+    sf::RectangleShape PAGE_RIGHT_BUTTON(sf::Vector2f(WINDOW_WIDTH/9.33, WINDOW_HEIGHT/12));
+    PAGE_RIGHT_BUTTON.setFillColor(sf::Color::Transparent);
+    PAGE_RIGHT_BUTTON.setOutlineColor(sf::Color::White);
+    PAGE_RIGHT_BUTTON.setOutlineThickness(2);
+    PAGE_RIGHT_BUTTON.setPosition((WINDOW_WIDTH/1.4)+(WINDOW_WIDTH/4.67)-(WINDOW_WIDTH/9.33)-(WINDOW_WIDTH/24)+6, (WINDOW_HEIGHT/1.4)+(WINDOW_HEIGHT/20)+(WINDOW_HEIGHT/12)+8);
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.key.code == sf::Keyboard::Enter) {
                 window.close();
+            }
         }
-
         window.clear();
-        window.draw(shape);
+        window.draw(SEARCH_MODE_SELECT_BOX);
+        window.draw(SEARCH_BOX);
+        window.draw(GO_BOX);
+        window.draw(RESULTS_MAIN_OUTLINE);
+        window.draw(PAGE_LEFT_BUTTON);
+        window.draw(PAGE_RIGHT_BUTTON);
         window.display();
-        i++;
     }
     return 0;
 }
