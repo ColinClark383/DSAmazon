8459b6c5126522e8a59f4f43ee4d4a6fc944d8db
Addison Ferrell <addisonf564@gmail.com>
2023-12-04 12:25:13 -0500

    updated hash files

--- hash.cpp
+++ hash.cpp
@@ -1,20 +1,44 @@
 #include "hash.h"
 #include <cmath>
 
-HashTable::HashTable(int capacity)
+HashTable::HashTable(int capacity, const std::vector<product> &prod)
 {
+    // initialize table with capacity and insert values from data sheet
     this->capacity = capacity;
     container.resize(capacity);
     size = 0;
+    for (int i = 0; i < prod.size(); i++)
+        insertProd(prod[i]);
+}
+
+void HashTable::rehash()
+{
+    capacity = capacity * 2;
+    std::vector< std::vector< product>> newContainer(capacity);
+
+    // Rehash all existing elements into the new table
+    for (long i = 0; i < container.size(); ++i)
+    {
+        for (long j = 0; j < container[i].size(); ++j)
+         {
+            long newIndex = hashFunc(container[i][j].name);
+            newContainer[newIndex].push_back(container[i][j]);
+         }
+    }
+
+    // Update the hash table with the new one
+    container = std::move(newContainer);
 }
 
 void HashTable::insertProd(product product)
 {
+    // rehash if load factor is exceeded
     if (size >= (capacity * maxLoadFactor))
-        return;
+        rehash();
 
     int index = hashFunc(product.name);
     auto &bucket = container[index];
+    // find corresponding bucket and add new product if not already in bucket
     for (int i = 0; i < bucket.size(); i++)
         if (bucket[i].name == product.name)
             return;
@@ -24,14 +48,16 @@ void HashTable::insertProd(product product)
 
 long HashTable::hashFunc(std::string name)
 {
+    // takes characters of string and multiples the chars by ascending powers of 31 for hash 
     long index = 0;
     for (int i = 0; i < name.size(); i++)
         index += name[i] * pow(31, i);
-    return name.size() % capacity;
+    return index % capacity;
 }
 
 std::vector<product> HashTable::getKeyword(std::string keyword)
 {
+    // searches through corresponding bucket for the given products name to find matches
     std::vector<product> matches;
     int index = hashFunc(keyword);
     auto &bucket = container[index];
